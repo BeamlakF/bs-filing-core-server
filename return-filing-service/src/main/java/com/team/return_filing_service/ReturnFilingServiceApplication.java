9ed55961b81a55e5f467d2e9d9ff72ec9ab57f36
BeamlakF <beamlakfekadu562@gmail.com>
2026-06-24 11:30:39 +0300

    feat: setting up main file structure

---
+++ return-filing-service/src/main/java/com/team/return_filing_service/ReturnFilingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.team.return_filing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnFilingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnFilingServiceApplication.class, args);
+	}
+
+}
---
+++ return-filing-service/src/test/java/com/team/return_filing_service/ReturnFilingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.return_filing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnFilingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
